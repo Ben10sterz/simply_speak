82d09404bf2134905b479da0e2524c6c4b6b22c4
Ben10sterz <bkelln77@gmail.com>
2021-11-09 14:03:20 -0400

    Calendar now works on Main page!!!!

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:simply_speak/database/entry_class.dart';
 import 'package:simply_speak/database/entry_class_dao.dart';
 import 'package:simply_speak/screens/first_prompt.dart';
 import 'package:simply_speak/screens/information_page.dart';
@@ -31,12 +32,21 @@ class _HomepageState extends State<Homepage> {
 
   @override
   initState() {
-    calendarList = [
+    facesList = [
       'assets/images/Red Smiley.png',
-      'assets/images/Red Orange Smiley.png'
+      'assets/images/Red Orange Smiley.png',
+      'assets/images/Orange Smiley.png',
+      'assets/images/Yellow Green Smiley.png',
+      'assets/images/Green Smiley.png',
     ];
+
+    _performSingleMonthFetch(DateTime.now().month.toString());
     final query = entryDao.getQuery();
   }
+
+  List<String> facesList = [];
+  List<String> ratingsList = [];
+  List<String> calendarList = [];
 
   PageController pageController = PageController(initialPage: 1);
   int currentIndex = 1;
@@ -45,7 +55,6 @@ class _HomepageState extends State<Homepage> {
 
   final EntryTestDao entryDao = EntryTestDao();
 
-  List<String> calendarList = [];
   List<String> currentDateForCalendar = [
     DateTime.now().year.toString(),
     DateTime.now().month.toString()
@@ -53,6 +62,36 @@ class _HomepageState extends State<Homepage> {
 
   bool checkEntryMadeToday() {
     return entryDao.wasEntryMadeTodayAlready();
+  }
+
+  void _performSingleMonthFetch(String month) {
+    FirebaseDatabase.instance
+        .reference()
+        .child(FirebaseAuth.instance.currentUser!.uid)
+        .once()
+        .then((snapshot) {
+      Map<dynamic, dynamic> values = snapshot.value;
+      for (var entries in values.values) {
+        final message = Entry.fromJson(entries);
+        var splitDate = message.date.split('-');
+
+        if (splitDate[1] == month) {
+          calendarList.add(message.date);
+          ratingsList.add((int.parse(message.rating) - 1).toString());
+        }
+        //print(message.date);
+      }
+      //final entries = Entry.fromJson(snapshot.value);
+      //print(entries.date.toString());
+    });
+  }
+
+  Future<bool> testFuture() async {
+    if (calendarList == []) {
+      return Future<bool>.value(false);
+    } else {
+      return Future<bool>.value(true);
+    }
   }
 
   // Stream<List> _initGetDatabase(List list) async {
@@ -113,74 +152,101 @@ class _HomepageState extends State<Homepage> {
           //     icon: ImageIcon(NetworkImage(user!.photoURL!)))
         ],
       ),
-      body: StreamBuilder(
-          stream: database.onValue,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              //print(snapshot.data!.toString());
-              return PageView(
-                controller: pageController,
-                onPageChanged: (page) {
-                  setState(() {
-                    currentIndex = page;
-                  });
-                },
-                children: [
-                  Container(
-                    color: Colors.amber,
-                    child: Column(
-                      children: [
-                        ElevatedButton(
-                          onPressed: beginEntry,
-                          child: Icon(
-                              // checkEntryMadeToday() ? Icons.mic :
-                              Icons.mic_off),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                      color: Colors.blue,
-                      child: TableCalendar(
-                          firstDay: DateTime.now(),
-                          lastDay:
-                              DateTime.now().add(const Duration(days: 5 * 365)),
-                          focusedDay: DateTime.now(),
-                          dayHitTestBehavior: HitTestBehavior.translucent,
-                          onDaySelected: (date, event) {
-                            print(date.toString());
-                          },
-                          onPageChanged: (date) {
-                            //_initGetDatabase(currentDateForCalendar);
-                            calendarList = [
-                              'assets/images/Red Smiley.png',
-                              'assets/images/Red Orange Smiley.png'
-                            ];
-                          },
-                          calendarBuilders: CalendarBuilders(
-                              defaultBuilder: (context, day, test) {
-                            final text = DateFormat.E().format(day);
+      body: PageView(
+        controller: pageController,
+        onPageChanged: (page) {
+          setState(() {
+            currentIndex = page;
+          });
+        },
+        children: [
+          Container(
+            color: Colors.amber,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: beginEntry,
+                  child: Icon(
+                      // checkEntryMadeToday() ? Icons.mic :
+                      Icons.mic_off),
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: FutureBuilder(
+                future: testFuture(),
+                builder: (context, snapshot) {
+                  if (snapshot.data == true) {
+                    return Center(
+                        heightFactor: 300.0,
+                        child: TableCalendar(
+                            rowHeight: 75.00,
+                            daysOfWeekHeight: 20.00,
+                            firstDay: DateTime(2021, 11, 1),
+                            lastDay: DateTime.now()
+                                .add(const Duration(days: 5 * 365)),
+                            focusedDay: DateTime.now(),
+                            dayHitTestBehavior: HitTestBehavior.translucent,
+                            onDaySelected: (date, event) {
+                              print(calendarList);
+                              //print(date.toString());
+                            },
+                            onPageChanged: (date) {
+                              print(date.month.toString());
+                              _performSingleMonthFetch(date.month.toString());
+                            },
+                            calendarBuilders: CalendarBuilders(
+                                defaultBuilder: (context, day, currentDay) {
+                              var i = 0;
+                              print(calendarList);
+                              for (var item in calendarList) {
+                                var splitDate = item.split('-');
 
-                            return Center(
-                              child: IconButton(
-                                icon: Image.asset(calendarList[1]),
-                                onPressed: () {
-                                  print("hit me");
-                                },
-                              ),
-                            );
-                          }))),
-                  Container(
-                    color: Colors.cyan,
-                    child: Image(image: NetworkImage(user!.photoURL!)),
-                  ),
-                ],
-                //       backgroundImage: NetworkImage(user.photoUrl!),
-              );
-            } else {
-              return CircularProgressIndicator();
-            }
-          }),
+                                // print("Wahat im looking for" +
+                                //     facesList[int.parse(ratingsList[i])]);
+
+                                // print(day.month);
+                                // print(splitDate[2]);
+
+                                if (day.year.toString() == splitDate[0]) {
+                                  if (day.month.toString() == splitDate[1]) {
+                                    if (day.day == int.parse(splitDate[2])) {
+                                      return Column(
+                                        children: [
+                                          IconButton(
+                                            icon: Image.asset(facesList[
+                                                int.parse(ratingsList[i])]),
+                                            onPressed: () {
+                                              print("Day 1: " +
+                                                  day.day.toString());
+                                              //print(calendarList[day.day]);
+                                              print("hit me");
+                                            },
+                                          ),
+                                          Text(day.day.toString())
+                                        ],
+                                      );
+                                    }
+                                  }
+                                }
+
+                                i++;
+                              }
+                            })));
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                }),
+          ),
+          Container(
+            color: Colors.cyan,
+            child: Image(image: NetworkImage(user!.photoURL!)),
+          ),
+        ],
+        //       backgroundImage: NetworkImage(user.photoUrl!),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () =>
             Navigator.of(context).pushReplacement(MaterialPageRoute(

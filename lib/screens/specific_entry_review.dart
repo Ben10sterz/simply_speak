09c87fef3a4179e6066828bbf8e4c46e9f6d1cbb
Ben10sterz <bkelln77@gmail.com>
2021-11-17 13:08:42 -0400

    Documenting specific entry rating.dart

--- lib/screens/specific_entry_review.dart
+++ lib/screens/specific_entry_review.dart
@@ -8,7 +8,7 @@ import '../database/entry_class_dao.dart';
 class SpecificEntryReview extends StatefulWidget {
   final EntryDao entryDao;
   final DateTime selectedDate;
-  SpecificEntryReview(
+  const SpecificEntryReview(
       {Key? key, required this.entryDao, required this.selectedDate})
       : super(key: key);
 
@@ -19,12 +19,14 @@ class SpecificEntryReview extends StatefulWidget {
 class _SpecificEntryReviewState extends State<SpecificEntryReview> {
   final user = FirebaseAuth.instance.currentUser;
 
+  // initializing our Entry object as empty
   Entry entry = Entry('', '', '', '', '', '');
   @override
   void initState() {
     _retrieveEntry(widget.selectedDate);
   }
 
+  // same as entry_review.dart, gets our users rating url for this entry
   String getIcon() {
     List<String> urlList = [
       'assets/images/Red Smiley.png',
@@ -36,13 +38,13 @@ class _SpecificEntryReviewState extends State<SpecificEntryReview> {
     return urlList[int.parse(entry.rating) - 1];
   }
 
-  //late Entry entry;
-
+  // function to wait half a second for our database to grab our entries
   Future<bool> testFuture() async {
     await Future.delayed(const Duration(seconds: 1), () => "5");
     return Future<bool>.value(true);
   }
 
+  // get the specific entry on the date provided
   void _retrieveEntry(DateTime selectedDate) {
     FirebaseDatabase.instance
         .reference()
@@ -58,7 +60,6 @@ class _SpecificEntryReviewState extends State<SpecificEntryReview> {
           if (int.parse(splitDate[1]) == selectedDate.month) {
             if (int.parse(splitDate[2]) == selectedDate.day) {
               entry = message;
-              print(entry.entryOne);
             }
           }
         }
@@ -70,7 +71,7 @@ class _SpecificEntryReviewState extends State<SpecificEntryReview> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Simply Speak'),
+          title: const Text('Simply Speak'),
         ),
         body: FutureBuilder(
             future: testFuture(),
@@ -78,44 +79,38 @@ class _SpecificEntryReviewState extends State<SpecificEntryReview> {
               if (snapshot.data == true) {
                 return SingleChildScrollView(
                   scrollDirection: Axis.vertical,
-                  physics: ScrollPhysics(),
-                  child: Container(
-                      child: Center(
+                  physics: const ScrollPhysics(),
+                  child: Center(
                     child: Column(
                       children: [
+                        //////////////////////////////////// Title area
                         Container(
                           height: 100.0,
                           width: 300.0,
-                          padding: EdgeInsets.only(top: 30),
-                          // decoration:
-                          //     BoxDecoration(border: Border.all(color: Colors.black)),
+                          padding: const EdgeInsets.only(top: 30),
                           child: (SingleChildScrollView(
                               scrollDirection: Axis.vertical,
-                              physics: ScrollPhysics(),
+                              physics: const ScrollPhysics(),
                               child: Text(
                                 entry.title,
-                                style: TextStyle(
+                                style: const TextStyle(
                                   fontSize: 25,
                                   decoration: TextDecoration.underline,
                                 ),
                                 textAlign: TextAlign.center,
                               ))),
                         ),
+                        ////////////////////////////////////////// Prompts container
                         Container(
                           height: 850.0,
                           width: 350.0,
-                          //padding: EdgeInsets.only(top: 0),
-                          // decoration:
-                          //     BoxDecoration(border: Border.all(color: Colors.black)),
                           child: Column(
                             children: [
+                              //////////////////////////////// 1st prompt
                               Container(
                                   height: 40.0,
                                   width: 330.0,
-                                  //padding: EdgeInsets.only(top: 0),
-                                  // decoration: BoxDecoration(
-                                  //     border: Border.all(color: Colors.black)),
-                                  child: Text(
+                                  child: const Text(
                                     "#1 Day Log Prompt",
                                     style: TextStyle(fontSize: 20),
                                     textAlign: TextAlign.center,
@@ -123,24 +118,23 @@ class _SpecificEntryReviewState extends State<SpecificEntryReview> {
                               Container(
                                 height: 100.0,
                                 width: 300.0,
-                                padding: EdgeInsets.only(top: 0),
+                                padding: const EdgeInsets.only(top: 0),
                                 decoration: BoxDecoration(
                                     border: Border.all(color: Colors.black)),
                                 child: (SingleChildScrollView(
                                     scrollDirection: Axis.vertical,
-                                    physics: ScrollPhysics(),
+                                    physics: const ScrollPhysics(),
                                     child: Text(entry.entryOne,
-                                        style: TextStyle(fontSize: 14),
+                                        style: const TextStyle(fontSize: 14),
                                         textAlign: TextAlign.center))),
                               ),
-                              Spacer(),
+                              const Spacer(),
+                              //////////////////////////////////////// 2nd prompt
                               Container(
                                   height: 60.0,
                                   width: 330.0,
-                                  padding: EdgeInsets.only(top: 20),
-                                  // decoration: BoxDecoration(
-                                  //     border: Border.all(color: Colors.black)),
-                                  child: Text(
+                                  padding: const EdgeInsets.only(top: 20),
+                                  child: const Text(
                                     "#2 Appreciation Prompt",
                                     style: TextStyle(fontSize: 20),
                                     textAlign: TextAlign.center,
@@ -148,24 +142,23 @@ class _SpecificEntryReviewState extends State<SpecificEntryReview> {
                               Container(
                                 height: 100.0,
                                 width: 300.0,
-                                padding: EdgeInsets.only(top: 0),
+                                padding: const EdgeInsets.only(top: 0),
                                 decoration: BoxDecoration(
                                     border: Border.all(color: Colors.black)),
                                 child: (SingleChildScrollView(
                                     scrollDirection: Axis.vertical,
-                                    physics: ScrollPhysics(),
+                                    physics: const ScrollPhysics(),
                                     child: Text(entry.entryTwo,
-                                        style: TextStyle(fontSize: 14),
+                                        style: const TextStyle(fontSize: 14),
                                         textAlign: TextAlign.center))),
                               ),
-                              Spacer(),
+                              const Spacer(),
+                              ////////////////////////////////////////// 3rd Prompt
                               Container(
                                   height: 60.0,
                                   width: 330.0,
-                                  padding: EdgeInsets.only(top: 20),
-                                  // decoration: BoxDecoration(
-                                  //     border: Border.all(color: Colors.black)),
-                                  child: Text(
+                                  padding: const EdgeInsets.only(top: 20),
+                                  child: const Text(
                                     "#3 Improvement Prompt",
                                     style: TextStyle(fontSize: 20),
                                     textAlign: TextAlign.center,
@@ -173,58 +166,51 @@ class _SpecificEntryReviewState extends State<SpecificEntryReview> {
                               Container(
                                 height: 100.0,
                                 width: 300.0,
-                                padding: EdgeInsets.only(top: 0),
+                                padding: const EdgeInsets.only(top: 0),
                                 decoration: BoxDecoration(
                                     border: Border.all(color: Colors.black)),
                                 child: (SingleChildScrollView(
                                     scrollDirection: Axis.vertical,
-                                    physics: ScrollPhysics(),
+                                    physics: const ScrollPhysics(),
                                     child: Text(entry.entryThree,
-                                        style: TextStyle(fontSize: 14),
+                                        style: const TextStyle(fontSize: 14),
                                         textAlign: TextAlign.center))),
                               ),
+                              ////////////////////////////////////// Rating icon image
                               Container(
                                   width: 330,
                                   height: 125,
-                                  // decoration: BoxDecoration(
-                                  //     border: Border.all(color: Colors.black)),
-                                  padding: EdgeInsets.only(top: 30),
+                                  padding: const EdgeInsets.only(top: 30),
                                   child: Image(
                                     width: 50,
                                     height: 50,
                                     image: AssetImage(getIcon()),
                                     //size: 50,
                                   )),
-                              Spacer(),
+                              const Spacer(),
+                              /////////////////////////////////////////////// back button at the bottom
                               Container(
                                   height: 100.0,
                                   width: 330.0,
-                                  padding: EdgeInsets.only(top: 0),
-                                  // decoration: BoxDecoration(
-                                  //     border: Border.all(color: Colors.black)),
+                                  padding: const EdgeInsets.only(top: 0),
                                   alignment: Alignment.bottomCenter,
                                   child: ElevatedButton(
                                       onPressed: () =>
                                           Navigator.of(context).pop(),
-                                      child: Icon(Icons.arrow_back_rounded),
+                                      child:
+                                          const Icon(Icons.arrow_back_rounded),
                                       style: ElevatedButton.styleFrom(
-                                          fixedSize: Size(75, 50))
-                                      // ElevatedButton(
-                                      //   onPressed: () => entryDao.checkForVal(),
-                                      //   child: Text('Check'),
-                                      // )
-                                      )),
-                              Spacer()
+                                          fixedSize: const Size(75, 50)))),
+                              const Spacer()
                             ],
                           ),
                         )
                       ],
                     ),
-                  )),
+                  ),
                 );
               } else {
-                print("doesn it ever ggo here");
-                return Center(child: CircularProgressIndicator());
+                return const Center(child: CircularProgressIndicator());
               }
             }));
   }
